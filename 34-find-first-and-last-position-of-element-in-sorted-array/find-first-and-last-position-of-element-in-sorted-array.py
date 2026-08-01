4f7cd0bd64d91d11a614122546f229c16319e4f9
ABHISHEK MAHESH MARIGERI <abhishekmarigeri9676@gmail.com>
2026-08-01 18:08:31 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (59.24%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,53 +1,45 @@
-
-
 class Solution:
-    def lowerBound(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        low, high = 0, n - 1
-        lb = -1
-
-        while low <= high:
-            mid = (low + high) // 2
-
-            if nums[mid] >= target:
-                lb = mid
-                high = mid - 1
-            else:
-                low = mid + 1
-
-        return lb
-
-    def upperBound(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        low, high = 0, n - 1
-        ub = -1
-
-        while low <= high:
-            mid = (low + high) // 2
-
-            if nums[mid] > target:
-                ub = mid
-                high = mid - 1
-            else:
-                low = mid + 1
-
-        return ub
-
     def searchRange(self, nums: List[int], target: int) -> List[int]:
 
-        if len(nums) == 0:
-            return [-1, -1]
+        def find_left(target,nums):
+            n=len(nums)
+            low=0
+            high=n-1
 
-        lb = self.lowerBound(nums, target)
+            while low<=high:
+                mid=(low+high)//2
+                if nums[mid] >= target:
+                    high=mid-1
+                else:
+                    low=mid+1
 
-        # Target does not exist
-        if lb == -1 or nums[lb] != target:
-            return [-1, -1]
+            return low
 
-        ub = self.upperBound(nums, target)
 
-        # Target is the last element(s) in the array
-        if ub == -1:
-            return [lb, len(nums) - 1]
+        def find_right(target,nums):
+            n=len(nums)
+            low=0
+            high=n-1
 
-        return [lb, ub - 1]
+            while low <= high:
+                mid=(low+high)//2
+                if nums[mid] == target:
+                    low=mid+1
+                elif nums[mid] < target:
+                    low = mid+1
+                else:
+                    high=mid-1
+            return high
+
+
+        left_most_element = find_left(target,nums)
+        right_most_element = find_right(target,nums)
+
+
+        if left_most_element <= right_most_element:
+            return [left_most_element,right_most_element]
+        else:
+            return [-1,-1]    
+
+
+        
